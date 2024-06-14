5d1b49fe951c9946fb20747805c89330e72d6d77
Jake-HBH <jakehersbach2005@gmail.com>
2024-06-14 13:55:28 +0200

    player fix 2

--- src/js/player.js
+++ src/js/player.js
@@ -3,7 +3,7 @@ import { Actor, CollisionType, Vector, Sprite, Axes, } from 'excalibur';
 
 export class Player extends Actor {
     constructor(x, y) {
-        super(x, y);
+        super({x, y});
         this.body.collisionType = CollisionType.Active;
         this.graphics.use(Resources.Seagull.toSprite())
         this.body.mass = 10;
@@ -11,17 +11,17 @@ export class Player extends Actor {
 
     }
 
-    onPreUpdate(engine) {
-        if (engine.mygamepad === null) {
-            console.log("error ahoele");
-            return;
-        }
-        const xValue = engine.mygamepad.getAxes(Axes.LeftStickX);
-        const yValue = engine.mygamepad.getAxes(Axes.LeftStickY);
-        this.vel = new Vector(xValue * 100, yValue * 100);
+    // onPreUpdate(engine) {
+    //     if (engine.mygamepad === null) {
+    //         console.log("error ahoele");
+    //         return;
+    //     }
+    //     const xValue = engine.mygamepad.getAxes(Axes.LeftStickX);
+    //     const yValue = engine.mygamepad.getAxes(Axes.LeftStickY);
+    //     this.vel = new Vector(xValue * 100, yValue * 100);
 
-        if (engine.mygamepad.isButtonPressed(Buttons.Face1)) {
-            console.log("jump!");
-        }
-    }
+    //     if (engine.mygamepad.isButtonPressed(Buttons.Face1)) {
+    //         console.log("jump!");
+    //     }
+    // }
 }
