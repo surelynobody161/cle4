24a3b439969463979b45048dbcd05c714a3caac2
surelynobody161 <surelynobody161@gmail.com>
2024-06-14 14:14:50 +0200

    arcade movement added

--- src/js/game.js
+++ src/js/game.js
@@ -1,5 +1,5 @@
 import '../css/style.css'
-import { Actor, Engine, Vector, DisplayMode, SolverStrategy } from "excalibur"
+import { Actor, Engine, Vector, DisplayMode, SolverStrategy, Buttons } from "excalibur"
 import { Resources, ResourceLoader } from './resources.js'
 import { Player } from './player.js';
 
@@ -30,7 +30,26 @@ export class Game extends Engine {
     onInitialize(engine) {
         const player = new Player(500, 450)
         this.add(player);
+
+        engine.input.gamepads.setMinimumGamepadConfiguration({
+            axis: 4,
+            buttons: 6,
+        });
+        engine.input.gamepads.enabled = true;
+
+        engine.input.gamepads.on('connect', (connectEvent) => {
+            console.log('controllers connected')
+            this.gamepadConnected = true;
+        });
+
+        setTimeout(() => {
+            if (!this.gamepadConnected) {
+                console.log('no controllers connected!')
+
+            }
+        }, 2000);
     }
+
 }
 
 new Game()
--- src/js/player.js
+++ src/js/player.js
@@ -1,9 +1,9 @@
 import { Resources } from './resources';
-import { Actor, CollisionType, Vector, Sprite, Axes, } from 'excalibur';
+import { Actor, CollisionType, Vector, Sprite, Axes, Input } from 'excalibur';
 
 export class Player extends Actor {
     constructor(x, y) {
-        super({x, y});
+        super({ x, y });
         this.body.collisionType = CollisionType.Active;
         this.graphics.use(Resources.Seagull.toSprite())
         this.body.mass = 10;
@@ -11,17 +11,15 @@ export class Player extends Actor {
 
     }
 
-    // onPreUpdate(engine) {
-    //     if (engine.mygamepad === null) {
-    //         console.log("error ahoele");
-    //         return;
-    //     }
-    //     const xValue = engine.mygamepad.getAxes(Axes.LeftStickX);
-    //     const yValue = engine.mygamepad.getAxes(Axes.LeftStickY);
-    //     this.vel = new Vector(xValue * 100, yValue * 100);
+    onPreUpdate(engine) {
+        if (engine.mygamepad === null) {
+            console.log("error ahoele");
+            return;
+        }
+        let xAxis = engine.input.gamepads.at(0).getAxes(Input.Axes.LeftStickX);
+        let yAxis = engine.input.gamepads.at(0).getAxes(Input.Axes.LeftStickY);
+        //this.vel = new Vector(xValue * 100, yValue * 100);
+        console.log(xAxis)
+    }
 
-    //     if (engine.mygamepad.isButtonPressed(Buttons.Face1)) {
-    //         console.log("jump!");
-    //     }
-    // }
 }
