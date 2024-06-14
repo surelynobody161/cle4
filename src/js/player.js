5cbca4e5ef281cf9699bf3d9096b6ead681d81f1
surelynobody161 <surelynobody161@gmail.com>
2024-06-14 12:09:02 +0200

    note modules added

--- src/js/player.js
+++ src/js/player.js
@@ -1,17 +1,23 @@
 export class Player extends Actor {
+    constructor() {
+        super();
+        this.body.collisionType = CollisionType.Active;
+        this.body.mass = 10;
+        this.vel = new Vector(0, 0);
+        this.scale = new Vector(0.18, 0.18);
+    }
+
+
 
     onPreUpdate(engine) {
         if (engine.mygamepad === null) {
-            console.log("er is geen gamepad")
+            console.log("error geen gamepad")
             return
         }
-
-        // bewegen
         const xValue = engine.mygamepad.getAxes(Axes.LeftStickX)
         const yValue = engine.myamepad.getAxes(Axes.LeftStickY)
         this.vel = new Vector(xValue * 100, yValue * 100)
 
-        // schieten / springen
         if (engine.mygamepad.isButtonPressed(Buttons.Face1)) {
             console.log("jump!")
         }
