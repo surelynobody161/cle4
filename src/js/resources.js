3b5b23395769f2e30229dcfee670afc9337ccf43
surelynobody161 <surelynobody161@gmail.com>
2024-06-18 14:26:12 +0200

    nuclear warfare

--- src/js/resources.js
+++ src/js/resources.js
@@ -3,7 +3,7 @@ import { ImageSource, Sound, Resource, Loader } from 'excalibur'
 const Resources = {
     Seagull: new ImageSource('images/gull.png'),
     Floor: new ImageSource('images/rock floor 2.png'),
-    LobbyBackground: new ImageSource('images/lobby real.png'),
+    LobbyBackground: new ImageSource('images/schiecentralebeter.png'),
 }
 
 const ResourceLoader = new Loader()
